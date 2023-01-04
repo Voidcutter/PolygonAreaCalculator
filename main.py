61dbdf81459776761e56bb3abd13c147179115b9
Yuriy Pashkin <Webcutter@yandex.ru>
2023-01-04 17:04:47 +0100

    added a Rectangle class

---
+++ main.py
@@ -0,0 +1,38 @@
+class Rectangle:
+
+    def __init__(self, width=5, height=10) -> None:
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+    
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * self.height + 2 * self.width
+    
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        if self.height > 50 or self.width > 50:
+            result = "Too big for picture."
+        else:
+            result = ''
+            for line in range(self.height):
+                result += '*' * self.width + '\n'
+        return result
+
+    def get_amount_inside(self, obj):
+        h_num = self.height // obj.height
+        w_num = self.width // obj.width
+        return h_num * w_num
+
+    def __str__(self):
+        temp = Rectangle(5, 10)
+        return temp.get_picture()
